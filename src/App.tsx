08899c01a8ffc63c346689419fb6ccfc034b2dab
LokiObahoshe <sondereggerbecca@gmail.com>
2026-05-13 16:00:45 -0500

    React Learning using Alerts and Buttons

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Button from "./components/Button.tsx";
+import Alert from "./components/Alert.tsx";
+import { useState } from "react";
+
+function App() {
+  const [alertVisible, setAlertVisibility] = useState(false);
+
+  return <div>
+    {alertVisible && <Alert onClose={() => setAlertVisibility(false)}>My alert</Alert>}
+    <Button onClick={() => setAlertVisibility(true)}>My Button</Button>
+  </div>
+}
+
+export default App;
