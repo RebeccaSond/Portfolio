c5f217b576476faf019762067603fbcd21c2dad2
LokiObahoshe <sondereggerbecca@gmail.com>
2026-05-14 18:31:08 -0500

    Updated README

--- src/pages/Other.tsx
+++ src/pages/Other.tsx
@@ -2,7 +2,7 @@ export default function Other() {
     return (
         <div className="container">
             <h1>Other</h1>
-            <p>Welcome to the other page.</p>
+            <p>Work in Progress!</p>
         </div>
     );
 }
